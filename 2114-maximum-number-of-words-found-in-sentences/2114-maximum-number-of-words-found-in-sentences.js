7d5498c7d282e6a9b33d67b38b71a6e82c8cbadd
Aswincodess <aswinkrishnak877@gmail.com>
2026-07-12 22:29:35 +0530

    Time: 2 ms (73.77%), Space: 56.1 MB (54.32%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+
+    let max = 0;
+
+    for (let i = 0; i < sentences.length; i++) {
+
+        let words = sentences[i].split(" ");
+        let count = words.length;
+
+        if (count > max) {
+            max = count;
+        }
+    }
+
+    return max;
+};
+
+console.log(
+    mostWordsFound([
+        "alice and bob love leetcode",
+        "i think so too",
+        "this is great thanks very much"
+    ])
+);
