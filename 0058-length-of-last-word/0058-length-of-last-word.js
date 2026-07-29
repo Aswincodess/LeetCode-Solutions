ee3c80de132e14277200332bcbeb590b34e831f2
Aswincodess <aswinkrishnak877@gmail.com>
2026-07-29 22:42:44 +0530

    Time: 0 ms (100%), Space: 54.2 MB (16.8%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let  word=s.trim().split(" ");
+
+    let lastWord=word[word.length - 1];
+    return lastWord.length; 
+};
+
+
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("luffy is still joyboy"));
