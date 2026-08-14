ee491ee3d3942daa779e780590cf58c5a8396103
Aswincodess <aswinkrishnak877@gmail.com>
2026-08-14 09:48:35 +0530

    Time: 2 ms (58.06%), Space: 63.9 MB (41.94%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.ts
@@ -0,0 +1,24 @@
+function getCommon(nums1: number[], nums2: number[]): number {
+
+
+let i=0;
+let j=0;
+
+while(i<nums1.length && j<nums2.length){
+    if(nums1[i]===nums2[j]){
+           return nums1[i];
+    }
+
+    if(nums2[j]>nums1[i]){
+        i++
+    }
+        else{
+            j++
+        
+    }
+         
+}
+return -1
+
+
+};
