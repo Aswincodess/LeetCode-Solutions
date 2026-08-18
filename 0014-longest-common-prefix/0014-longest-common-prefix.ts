def8874795ff6bbe06f3ad9929ef58370ed5969e
Aswincodess <aswinkrishnak877@gmail.com>
2026-08-18 09:56:06 +0530

    Time: 2 ms (53.84%), Space: 54.5 MB (92.51%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -0,0 +1,23 @@
+function longestCommonPrefix(strs: string[]): string {
+
+    if (strs.length === 0) {
+        return "";
+    }
+
+    let prefix = "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+
+        for (let j = 1; j < strs.length; j++) {
+
+            if (i >= strs[j].length || strs[j][i] !== strs[0][i]) {
+                return prefix;
+            }
+
+        }
+
+        prefix += strs[0][i];
+    }
+
+    return prefix;
+}
