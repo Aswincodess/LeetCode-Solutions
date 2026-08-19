f7bcfdc56897773ac85a69534098546c4c6264bf
Aswincodess <aswinkrishnak877@gmail.com>
2026-08-19 22:27:51 +0530

    Time: 3 ms (91.13%), Space: 61.8 MB (47.75%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,28 @@
+function romanToInt(s: string): number {
+
+    const values: { [key: string]: number } = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        let current = values[s[i]];
+        let next = values[s[i + 1]];
+
+        if (current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
