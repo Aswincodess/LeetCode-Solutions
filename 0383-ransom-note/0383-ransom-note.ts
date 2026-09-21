c9473051267d68d75d2614f65eae8d8300993b77
Aswincodess <aswinkrishnak877@gmail.com>
2026-09-21 09:37:43 +0530

    Time: 3 ms (97.72%), Space: 59.2 MB (60.85%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -0,0 +1,24 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+
+    let magazineArr: string[] = magazine.split("");
+
+    for (let i = 0; i < ransomNote.length; i++) {
+
+        let found: boolean = false;
+
+        for (let j = 0; j < magazineArr.length; j++) {
+
+            if (ransomNote[i] === magazineArr[j]) {
+                magazineArr[j] = "";
+                found = true;
+                break;
+            }
+        }
+
+        if (found === false) {
+            return false;
+        }
+    }
+
+    return true;
+}
